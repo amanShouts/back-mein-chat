81f9ec82f43963cfcdff977a64784d2927985bd2
Aman Singh <aman.shouts@gmail.com>
2024-04-16 23:25:42 +0530

    added express.json() middleware

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json())
 const PORT = 3000;
 const http = require('http');
 const WebSocketServer = require('websocket').server
